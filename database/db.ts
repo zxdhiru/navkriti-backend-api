c192b43fa3605e8b6ca1649a672bcb3389459938
Dhiru <dhiru@alljobguider.in>
2024-12-20 00:25:25 +0530

    simplify database connection log output

--- database/db.ts
+++ database/db.ts
@@ -3,7 +3,7 @@ import mongoose from "mongoose";
 export default async function connectToDatabase(url: string) {
   try {
     const connection = await mongoose.connect(url);
-    console.log("Database connection established", connection.connection.host, connection.connection.collection);
+    console.log("Database connection established", connection.connection.host);
   } catch (error) {
     console.log("Database connection failed", error);
   }
