5a6ac574669d21bfc6349eb7a0067357d2320102
Dhiru <dhiru@alljobguider.in>
2024-12-20 03:17:30 +0530

    add login and logout routes to user router

--- user/routes/route.ts
+++ user/routes/route.ts
@@ -1,10 +1,13 @@
 import { Router } from "express";
-import handleUserSignup from "../controller/userController";
+import {handleUserLogin, handleUserLogout, handleUserSignup} from "../controller/userController";
+import { setRequestUser } from "../../middlewares/setRequest";
 
 const UserRouter = Router();
 
 // Route for user registration
 UserRouter.post("/register",handleUserSignup);
+UserRouter.post("/login",handleUserLogin);
+UserRouter.post("/logout",setRequestUser, handleUserLogout);
 
 
 export default UserRouter;
