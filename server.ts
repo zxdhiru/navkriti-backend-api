d7b8c7a26b9dcd8806548a4d50a9b51eb814d917
Dhiru <dhiru@alljobguider.in>
2024-12-20 03:17:48 +0530

    add cookie parser middleware to server configuration

--- server.ts
+++ server.ts
@@ -4,11 +4,13 @@ dotenv.config();
 import connectToDatabase from "./database/db";
 import cors from "cors";
 import UserRouter from "./user/routes/route";
+import cookieParser from "cookie-parser";
 
 const app = express();
 const port = process.env.PORT || 3000;
 
 // middleware
+app.use(cookieParser());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use((req, _, next) => {
