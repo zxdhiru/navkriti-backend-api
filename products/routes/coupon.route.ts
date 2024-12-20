0615776a126b405939fb708c0a47c5c251306a1e
Dhiru <dhiru@alljobguider.in>
2024-12-20 12:33:18 +0530

    Add comment to indicate protected routes in coupon router

--- products/routes/coupon.route.ts
+++ products/routes/coupon.route.ts
@@ -2,7 +2,7 @@ import {Router } from 'express';
 import { handleAddCoupon } from '../controller/coupon.controller';
 
 const couponRoute = Router();
-
+// PROTECTED ROUTES
 couponRoute.post('/', handleAddCoupon)
 
 export default couponRoute
