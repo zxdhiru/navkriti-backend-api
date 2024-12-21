df2d91390ff77814b7b76bc131ee56aed9e9e97d
Dhiru <dhiru@alljobguider.in>
2024-12-21 20:14:24 +0530

    Add access token options to constants for improved token management

--- utils/constants.ts
+++ utils/constants.ts
@@ -1,8 +1,14 @@
-export const cookieOptions : any = {
+export const refreshTokenOptions : any = {
     httpOnly: true,
     sameSite: 'lax',
     secure: process.env.NODE_ENV === 'production',
     maxAge: 1000 * 60 * 60 * 24 * 7
+};
+export const accessTokenOptions : any = {
+    httpOnly: true,
+    sameSite: 'lax',
+    secure: process.env.NODE_ENV === 'production',
+    maxAge: 1000 * 60 * 60
 };
 // Type for user data in the request
 export interface UserRequest extends Request {
