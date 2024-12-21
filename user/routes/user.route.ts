ab6e56e9cf80896e5110bdcf43a98393709efb87
Dhiru <dhiru@alljobguider.in>
2024-12-22 00:16:21 +0530

    Add admin check to user detail route for enhanced security

--- user/routes/user.route.ts
+++ user/routes/user.route.ts
@@ -1,6 +1,6 @@
 import { Router } from "express";
 import {handleGetAllUsers, handleGetSingleUser, handleGetUserProfile, handleUserLogin, handleUserLogout, handleUserSignup, handleVerifyUser} from "../controller/user.controller";
-import { setRequestUser } from "../../middlewares/setRequest";
+import { checkForAdmin, setRequestUser } from "../../middlewares/setRequest";
 
 const UserRouter = Router();
 
@@ -13,7 +13,7 @@ UserRouter.post("/logout",setRequestUser, handleUserLogout);
 // Route for fetching user details
 UserRouter.get("/me",setRequestUser, handleGetUserProfile); 
 UserRouter.get("/all", handleGetAllUsers) //protected route
-UserRouter.get("/:id", handleGetSingleUser); //protected route
+UserRouter.get("/:id", setRequestUser, checkForAdmin, handleGetSingleUser); //protected route
 
 
 export default UserRouter;
