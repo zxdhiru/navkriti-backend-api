d235f33c7fd37a9e6d01a848e3c3c20c3417e4c1
Dhiru <dhiru@alljobguider.in>
2024-12-21 21:34:20 +0530

    Add user verification route to handle user verification requests

--- user/routes/user.route.ts
+++ user/routes/user.route.ts
@@ -1,11 +1,12 @@
 import { Router } from "express";
-import {handleGetAllUsers, handleGetSingleUser, handleGetUserProfile, handleUserLogin, handleUserLogout, handleUserSignup} from "../controller/user.controller";
+import {handleGetAllUsers, handleGetSingleUser, handleGetUserProfile, handleUserLogin, handleUserLogout, handleUserSignup, handleVerifyUser} from "../controller/user.controller";
 import { setRequestUser } from "../../middlewares/setRequest";
 
 const UserRouter = Router();
 
 // Route for user registration
 UserRouter.post("/register",handleUserSignup);
+UserRouter.post("/verify",setRequestUser,handleVerifyUser);
 UserRouter.post("/login",handleUserLogin);
 UserRouter.post("/logout",setRequestUser, handleUserLogout);
 
