3964d53324536eb68c62f38c5745729740462d98
Dhiru <dhiru@alljobguider.in>
2024-12-23 15:03:15 +0530

    Refactor handleAddCoupon function for improved readability and validation

--- products/controller/coupon.controller.ts
+++ products/controller/coupon.controller.ts
@@ -3,11 +3,48 @@ import { asyncHandler } from "../../utils/asyncHandler";
 import { Coupon } from "../model/coupon.model";
 import { ApiResponse } from "../../utils/apiResponse";
 
-export const handleAddCoupon = asyncHandler(async (req: Request, res: Response) => {
-    const {code, description, discountType, discountValue, minOrderValue, maxDiscount, startDate, expiresAt, isActive, usageLimit} = req.body;
-    if(!code || !description || !discountType || !discountValue || !minOrderValue || !maxDiscount || !startDate || !expiresAt || !isActive || !usageLimit) {
-        return res.status(400).json({ message: 'All fields are required' });
+export const handleAddCoupon = asyncHandler(
+    async (req: Request, res: Response) => {
+        const {
+            code,
+            description,
+            discountType,
+            discountValue,
+            minOrderValue,
+            maxDiscount,
+            startDate,
+            expiresAt,
+            isActive,
+            usageLimit,
+        } = req.body;
+        if (
+            !code ||
+            !description ||
+            !discountType ||
+            !discountValue ||
+            !minOrderValue ||
+            !maxDiscount ||
+            !startDate ||
+            !expiresAt ||
+            !isActive ||
+            !usageLimit
+        ) {
+            return res.status(400).json({ message: "All fields are required" });
+        }
+        const coupon = await Coupon.create({
+            code,
+            description,
+            discountType,
+            discountValue,
+            minOrderValue,
+            maxDiscount,
+            startDate,
+            expiresAt,
+            isActive,
+            usageLimit,
+        });
+        return res
+            .status(201)
+            .json(new ApiResponse(201, coupon, "Coupon created successfully"));
     }
-    const coupon = await Coupon.create({code, description, discountType, discountValue, minOrderValue, maxDiscount, startDate, expiresAt, isActive, usageLimit});
-    return res.status(201).json( new ApiResponse(201, coupon, 'Coupon created successfully'));
-})
+);
