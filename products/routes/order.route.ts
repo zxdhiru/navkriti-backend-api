b1f40adfe820a18a5c4e30192b4c4e32ec834ab0
Dhiru <dhiru@alljobguider.in>
2024-12-23 15:04:58 +0530

    Fix formatting issues in order controller and route files

--- products/routes/order.route.ts
+++ products/routes/order.route.ts
@@ -4,6 +4,7 @@ import { setRequestUser } from "../../middlewares/setRequest";
 
 const orderRoute = Router()
 
+// orderRoute.post('/create-session', setRequestUser, )
 orderRoute.post('/', setRequestUser, handleCreateOrder)
 
 export default orderRoute
