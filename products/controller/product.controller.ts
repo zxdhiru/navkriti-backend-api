7ba30282c33c231975cd122d472d7ce23b694f88
Dhiru <dhiru@alljobguider.in>
2024-12-23 19:09:53 +0530

    Add product retrieval handlers for fetching all products and a single product by slug

--- products/controller/product.controller.ts
+++ products/controller/product.controller.ts
@@ -26,3 +26,21 @@ export const handleAddProduct = asyncHandler(async (req: Request, res: Response,
         new ApiError(500, error.message); // Pass the error to global error handler
     }
 });
+
+
+export const handleGetProducts = asyncHandler(async (req: Request, res: Response) => {
+    const products = await Product.find();
+    return res.status(200).json(new ApiResponse(200, products, "Products retrieved successfully"));
+}) 
+
+export const handleGetSingleProduct = asyncHandler(async (req: Request, res: Response) => {
+    const { slug } = req.params;
+    if(!slug) {
+        return res.status(400).json(new ApiResponse(400, null, "Product slug is required"));
+    }
+    const product = await Product.findOne({ slug });
+    if(!product) {
+        return res.status(404).json(new ApiResponse(404, null, "Product not found"));
+    }
+    return res.status(200).json(new ApiResponse(200, product, "Product retrieved successfully"));
+})
