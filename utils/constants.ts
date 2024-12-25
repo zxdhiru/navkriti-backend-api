96cf355bc7a922634af28cf209af3a1549b10107
Dhiru <dhiru@alljobguider.in>
2024-12-26 00:30:07 +0530

    Add clearCookieOptions with explicit typing to constants

--- utils/constants.ts
+++ utils/constants.ts
@@ -1,3 +1,5 @@
+import { CookieOptions } from "express";
+
 export const refreshTokenOptions : any = {
     httpOnly: true,
     sameSite: 'lax',
@@ -10,6 +12,12 @@ export const accessTokenOptions : any = {
     secure: process.env.NODE_ENV === 'production',
     maxAge: 1000 * 60 * 60
 };
+export const clearCookieOptions : CookieOptions = {
+    httpOnly: true,
+    sameSite: 'lax', // Explicitly typed as "lax"
+    secure: process.env.NODE_ENV === 'production',
+};
+
 // Type for user data in the request
 export interface UserRequest extends Request {
     user: { _id: string }; // Define the expected shape of req.user
