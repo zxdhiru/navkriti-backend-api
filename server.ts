5c56a3c87e465c6e72b62f960477ef1ee00649b8
Dhirendra Kumar Rana <dhiru@alljobguider.in>
2025-02-03 09:48:12 +0530

    Add new frontend url

--- server.ts
+++ server.ts
@@ -62,7 +62,7 @@ if (cluster.isPrimary) {
   }
 
   const corsOptions = {
-    origin: ["http://localhost:5173", "https://navkriti-frontend.onrender.com"],
+    origin: ["http://localhost:5173", "https://navkriti-frontend.onrender.com", "https://navkriti.onrender.com"],
     methods: ["GET", "POST", "PUT", "DELETE"],
     allowedHeaders: ["Content-Type", "Authorization"],
     credentials: true,
